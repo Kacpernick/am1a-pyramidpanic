2acead3832b7c650388a49ffd6fef47dcc291e9f
Kacpernick <Kacpernick@hotmail.com>
2013-11-25 14:43:40 +0100

    Update2

---
+++ PyramidPanic/PyramidPanic/GameScenes/StartScene/StartScene.cs
@@ -0,0 +1,51 @@
+﻿// Met using kan je een xna codebibliotheek toevoegen
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace PyramidPanic
+{
+    class StartScene
+    {
+        //Construstor van de StartScene
+        public StartScene()
+        {
+
+        }
+
+        //Geeft startward
+        public void Initialize()
+        {
+
+        }
+
+        //Deze methode maakt nieuwe objecten
+        public void LoadContent()
+        {
+
+        }
+
+        //Deze methode word normaal 60 maal per seconde aangeroepen.
+        public void Update()
+        {
+
+        }
+
+        //Deze methode word normaal 60 maal per seconde aangeroepen en tekent de textures op het canvas.
+        public void Draw()
+        {
+
+        }
+    }
+}
